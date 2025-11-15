d538b3434ff94de44d48c8035432f5d0cf31a40c
frat-karatasoglu <fratkaratasoglu23@hotmail.com>
2025-11-15 21:42:16 +0500

    fnish

---
+++ KaratasogluSurvey/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace KaratasogluSurvey;
+
+public partial class MainPage : ContentPage
+{
+    private double _age = 18;
+
+    public double Age
+    {
+        get => _age;
+        set
+        {
+            if (_age != value)
+            {
+                _age = value;
+                OnPropertyChanged(nameof(Age));
+                OnPropertyChanged(nameof(AgeDisplay));
+            }
+        }
+    }
+
+    public string AgeDisplay => $"Возраст: {Age:F0}";
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    private void SubmitButton_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Спасибо!", "Ваша анкета отправлена.", "OK");
+    }
+}
---
+++ KaratasogluSurvey/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KaratasogluSurvey
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
